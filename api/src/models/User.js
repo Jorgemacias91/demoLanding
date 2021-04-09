1abf6ee90ea842ce8bc2ea9d20f84f5b1e8bc5f9
jorge <jmaciasvesga@gmail.com>
2021-04-08 22:38:46 -0500

    rutas y modelado

---
+++ api/src/models/User.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('user', {
+    username : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: {
+          args: ["(?=.*[0-9])(?=.*[0-9])"],
+          msg:
+            "Password must contain at least 1 uppercase alphabetical character and 1 numeric character",
+        },
+      },
+    },
+    profilePic : {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    email: {
+      type : DataTypes.STRING,
+      allowNull:false
+    },
+    birth : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+  },{timestamps : false});
+};
